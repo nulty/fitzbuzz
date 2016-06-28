2acb7d8a26970cd74b4700db06bf3611a5b283df
Iain McNulty <iainmcnulty@gmail.com>
2016-06-28 09:05:23 +0100

    Add pagination on FizzBuzz homepage index

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,12 +1,12 @@
 class HomepageController < ApplicationController
   def show
     # @fizzbuzzs = FizzBuzz.fetch(number_params)
-    @fizzbuzzs = FizzBuzz.all
+    @fizzbuzzs = FizzBuzz.paginate(page: params[:page], per_page: params[:per_page])
   end
 
   private
 
   def number_params
-    params.permit(:numbers)
+    params.permit(:numbers, :page, :per_page)
   end
 end
