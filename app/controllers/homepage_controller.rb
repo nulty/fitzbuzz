b7711a72a5a16943848e6d2d277bf42cdd5582de
Iain McNulty <iainmcnulty@gmail.com>
2016-06-28 12:14:18 +0100

    homepage controller generates new FizzBuzz on demand

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,16 +1,30 @@
 class HomepageController < ApplicationController
   def show
-    # @fizzbuzzs = FizzBuzz.fetch(number_params)
-    @fizzbuzzs = FizzBuzz.paginate(page: params[:page], per_page: params[:per_page])
-
-    # allow the pagination Next button to be available despite the next entries
-    # not actually existing
-    @fizzbuzzs.total_entries = FizzBuzz::LIMIT
+    @fizzbuzzs = FizzBuzz.paginate(pagination_options)
+    prepare_fizzbuzzs
   end
 
   private
 
   def number_params
     params.permit(:numbers, :page, :per_page)
+  end
+
+  def prepare_fizzbuzzs
+    arel = @fizzbuzzs.arel
+    from_number = arel.offset.next
+    to_number = (arel.offset + arel.limit)
+
+    FizzBuzz.select_or_create_by_numbers((from_number..to_number).to_a)
+
+    @fizzbuzzs = FizzBuzz.where(number: (from_number..to_number)).paginate(pagination_options).offset(0)
+  end
+
+  def pagination_options
+    {
+      total_entries: FizzBuzz::LIMIT,
+      page: params[:page],
+      per_page: params[:per_page]
+    }
   end
 end
--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -6,5 +6,18 @@ RSpec.describe HomepageController, type: :controller do
       get :show
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders the show template' do
+      get :show
+      expect(response).to have_rendered(:show)
+    end
+
+    describe 'pagination' do
+      it 'creates new FizzBuzz when they don\'t exists' do
+        expect do
+          get :show, per_page: 5, page: 10
+        end.to change { FizzBuzz.count }.by(5)
+      end
+    end
   end
 end
