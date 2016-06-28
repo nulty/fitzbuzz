63d38850c7cf3a33f7989d7eb85248f01373047a
Iain McNulty <iainmcnulty@gmail.com>
2016-06-28 09:35:23 +0100

    Pagination Next pages available up to 1_000_000_000 FizzBuzz

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,6 +2,10 @@ class HomepageController < ApplicationController
   def show
     # @fizzbuzzs = FizzBuzz.fetch(number_params)
     @fizzbuzzs = FizzBuzz.paginate(page: params[:page], per_page: params[:per_page])
+
+    # allow the pagination Next button to be available despite the next entries
+    # not actually existing
+    @fizzbuzzs.total_entries = FizzBuzz::LIMIT
   end
 
   private
--- app/models/fizz_buzz.rb
+++ app/models/fizz_buzz.rb
@@ -1,2 +1,3 @@
 class FizzBuzz < ActiveRecord::Base
+  FizzBuzz::LIMIT = 1_000_000_000
 end
